65a3b228f15e773361f937028e5dc9334d7ab224
chapz <germainchi43@gmail.com>
2020-05-02 09:43:43 -0500

    error fix

--- db/schema.sql
+++ db/schema.sql
@@ -1,7 +1,7 @@
-DROP DATABASE IF EXISTS Burger_db;
+DROP DATABASE IF EXISTS burger_db;
 -- Creates the "blogger" database --
-CREATE DATABASE Burger_db;
-Use Burger_db;
+CREATE DATABASE burger_db;
+Use burger_db;
 Create table burgers (
   id int not null auto_increment,
   burger_name varchar(100) not null,
