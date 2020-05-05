31ec46c67b835804c162c5ee7af2aa7da8b2d2b8
chapz <germainchi43@gmail.com>
2020-05-05 17:09:59 -0500

    Deploying on heroku

--- db/schema.sql
+++ db/schema.sql
@@ -1,10 +1,9 @@
 DROP DATABASE IF EXISTS burger_db;
--- Creates the "blogger" database --
 CREATE DATABASE burger_db;
 Use burger_db;
-Create table burgers (
-  id int not null auto_increment,
-  burger_name varchar(100) not null,
+Create TABLE burgers (
+  id INT NOT NULL auto_increment,
+  burger_name VARCHAR(100) not null,
   devoured BOOLEAN DEFAULT False,
   PRIMARY KEY(id)
 );
